bea7917efd01c0a587fc6ac5950b87ea3b2f9695
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-03-17 03:35:59 +0300

    not finish

--- src/main.js
+++ src/main.js
@@ -1,24 +1,64 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import izitoast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 
-setupCounter(document.querySelector('#counter'))
+const apiKey = "49228326-f0295c59acbd8047419a0b87e";
+const input = document.querySelector(".input");
+const button = document.querySelector(".button");
+const gallery = document.querySelector(".gallery");
+
+button.addEventListener("click", () => {
+  const inputValue = input.value;
+  if (inputValue === "") {
+    izitoast.info({
+      title: "Bilgi",
+      message: "Lütfen aramak istediğiniz kelimeyi yazınız.",
+    });
+    return;
+  }
+  gallery.innerHTML = "<div class='loading-spinner'></div>";
+  fetch(
+    `https://pixabay.com/api/?key=${apiKey}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      gallery.innerHTML = "";
+      if (result.hits.length === 0) {
+        izitoast.error({
+          title: "Hata",
+          message: "Üzgünüz, aramanızla eşleşen resim yoktur.",
+        });
+        return;
+      }
+
+      result.hits.forEach((element) => {
+        const galleryItem = document.createElement("a");
+        galleryItem.href = element.largeImageURL;
+        galleryItem.classList.add("gallery-item");
+
+        const img = document.createElement("img");
+
+        img.src = element.webformatURL;
+        img.alt = element.tags;
+        img.classList.add("img");
+        gallery.appendChild(img);
+        gallery.appendChild(galleryItem);
+      });
+      izitoast.success({
+        title: "Başarılı",
+        message: `${result.hits.length} görsel bulundu.`,
+      });
+      const lightbox = new SimpleLightbox(".gallery a");
+      lightbox.refresh();
+    })
+
+    .catch((err) => {
+      console.log("Hata oluştu", err);
+      izitoast.error({
+        title: "Hata",
+        message: "Bir hata oluştu, lütfen tekrar deneyin.",
+      });
+    });
+});
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src", // Vite'nin proje kök dizini olarak 'src' klasörünü kullanmasını sağlar
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+});
